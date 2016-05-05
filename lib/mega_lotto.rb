805c6c7c48414ee519ff4bd02bbf44d522ae8765
Hillary <hillfeller@gmail.com>
2016-05-05 16:36:28 -0400

    First commit

---
+++ lib/mega_lotto.rb
@@ -0,0 +1,10 @@
+require "mega_lotto/version"
+require "mega_lotto/drawing"
+
+begin
+  require "pry"
+rescue LoadError
+end
+
+module MegaLotto
+end
---
+++ spec/mega_lotto_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe MegaLotto do
+  it 'has a version number' do
+    expect(MegaLotto::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(true).to eq(true)
+  end
+end
